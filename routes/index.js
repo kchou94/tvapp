35b43012a2e4bfb2b3a38d464dae2b0e21a4bc10
Kevin Chou <kchou94@gmail.com>
2018-03-13 23:35:30 -0400

    add dynamic page title

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ var router = express.Router();
 
 /* INDEX */
 router.get('/', function(req, res) {
-  res.render('index', {page: 'home'});
+  res.render('index', {page: 'Home'});
 });
 
 module.exports = router;
--- routes/shows.js
+++ routes/shows.js
@@ -8,7 +8,7 @@ var router = express.Router();
 
 /* INDEX */
 router.get('/', function(req, res) {
-  res.render('shows/index', {page: 'shows'});
+  res.render('shows/index', {page: 'Shows'});
 });
 
 module.exports = router;
